8eba7e1721dea92a9a8de17caf7608fa1d7f8abd
Dhairya Narang <dhairyanarang1@gmail.com>
2026-09-11 21:32:17 +0530

    Add ProbH.c for integer and long long calculations

---
+++ Contest #1/ProbH.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    long long a;
+    long long b;
+    long long c;
+    long long x;
+    
+    scanf("%lld %lld %lld",&a,&b,&c);
+    x = ((a*b)/c);
+    if((a*b)%c > 0){
+        printf("double");
+    }else if(x <= 2147483647 && x >= (-2147483648 )){
+        printf("int");
+    }else {
+        printf("long long");
+    }
+ 
+}

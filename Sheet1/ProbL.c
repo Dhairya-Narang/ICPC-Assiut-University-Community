f6143307502c56cbee026e753656179c45d6359e
Dhairya Narang <dhairyanarang1@gmail.com>
2026-09-15 09:30:04 +0530

    Modify program to compare strings instead of integers
    Replaced integer comparison with string comparison to check if two strings are equal.

--- Sheet1/ProbL.c
+++ Sheet1/ProbL.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
-    int x , y;
-    scanf("%d %d", &x , &y);
-    if (x >= y)
-        printf("Yes");
-    else
-        printf("No");
+    char a[100],b[100],c[100],d[100];
+    scanf("%s %s\n %s %s",&a,&b,&c,&d);
+    if (strcmp(b,d)==0){
+        printf("ARE Brothers");
+    }else{
+        printf("NOT");
+    }
+    
 }
